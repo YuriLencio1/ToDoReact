81ecb7a701e6f8026c61cab05f547a858654f8cd
Yuri Eduardo Lencio <yurilencio@ciandt.com>
2025-11-12 16:32:58 -0300

    adicionando botões a lista de tarefas

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -30,6 +30,11 @@ function App() {
       <div className="todo">
         <div className="content">
           <p> {todo.text } </p>
+          <p className="category"> ({todo.category}) </p>
+        </div>
+        <div>
+          <button>completar</button>
+          <button>X</button>
         </div>
       </div> ) }
     </div>
