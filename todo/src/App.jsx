fe2b915b7db02affd0111d0f7cb6c0e81589333c
Yuri Eduardo Lencio <yurilencio@ciandt.com>
2025-11-13 10:57:04 -0300

    adicionando componentes e imagem

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,46 +1,40 @@
-import { useState } from 'react'
+import { useState } from "react";
+import "./App.css"
+import "./components/Todo.jsx"
+import Todo from "./components/Todo.jsx";
+
 
 function App() {
   const [todos, setTodos] = useState([
     {
-      id:1,
+      id: 1,
       text: "criar funcionalidade x no sistema",
       category: "Trabalho",
       isCompleted: false,
     },
     {
-      id:2,
+      id: 2,
       text: "Ir pra academia",
       category: "Pessoal",
       isCompleted: false,
     },
     {
-      id:3,
+      id: 3,
       text: "Estudar React",
       category: "Estudos",
       isCompleted: false,
-    }
-  ])
+    },
+  ]);
 
-  return <div className="app">  
-  
-    <h1>Lista de Tarefas</h1>
-    <div className="todo-list">
-      {todos.map((todo) =>
-      <div className="todo">
-        <div className="content">
-          <p> {todo.text } </p>
-          <p className="category"> ({todo.category}) </p>
-        </div>
-        <div>
-          <button>completar</button>
-          <button>X</button>
-        </div>
-      </div> ) }
+  return (
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <div className="todo-list">
+        {todos.map((todo) => (
+         <Todo todo={todo} />
+        ))}
+      </div>
     </div>
-
-
-  </div>
-
+  );
 }
 export default App;
